60dc3f25710e9cdf5ff3dc31d8d425418ec98a06
Alan Gomes <alangssilva@gmail.com>
2023-06-20 01:37:43 -0300

    feat - add routers

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,49 @@
 import * as React from "react"
 import CenterFooter from "./components/globals/CenterFooter"
-import Header from "./components/globals/Header"
-
-import Home from "./components/pages/Home"
-
 import {
   ChakraProvider,
-
   theme,
 } from "@chakra-ui/react"
 
+import Header from "./components/globals/Header"
+import { createBrowserRouter, RouterProvider } from "react-router-dom"
+
+import Home from "./pages/Home"
+import Contact from "./pages/Contact"
+import AboutMe from "./pages/AboutMe"
+import ProjectDetails from "./pages/ProjectDetails"
+import Projects from "./pages/Projects"
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />
+  },
+  {
+    path: "Contact",
+    element: <Contact />
+  },
+  {
+    path: "AboutMe",
+    element: <AboutMe />
+  },
+  {
+    path: "ProjectDetails/:id",
+    element: <ProjectDetails />
+  },
+  {
+    path: "Projects",
+    element: <Projects />
+  },
+]);
 
 export const App = () => (
   <ChakraProvider theme={theme}>
     <Header></Header>
-    <Home></Home>
+    <RouterProvider router={router} />
+
+
     <CenterFooter></CenterFooter>
   </ChakraProvider>
 )
