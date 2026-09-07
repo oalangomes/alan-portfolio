04ebcce46d6f7f9d982a24801ef97c0d1a47237f
Alan Gomes <52175262+oalangomes@users.noreply.github.com>
2026-09-07 10:53:07 -0300

    test: run Home under router context

--- src/__tests__/Home.test.tsx
+++ src/__tests__/Home.test.tsx
@@ -1,8 +1,14 @@
 import { render, screen } from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom';
 import Home from '../pages/Home';
 
 test('renders the portfolio positioning', () => {
-  render(<Home />);
+  render(
+    <MemoryRouter>
+      <Home />
+    </MemoryRouter>,
+  );
+
   expect(
     screen.getByRole('heading', {
       name: /I design systems that make complex software easier to build, understand and evolve/i,
