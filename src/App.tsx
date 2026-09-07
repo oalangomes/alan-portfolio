8777129fc287790abe6fc648c2c55deb126afe2a
Alan Gomes <52175262+oalangomes@users.noreply.github.com>
2026-09-07 10:51:21 -0300

    fix: make routing GitHub Pages safe

--- src/App.tsx
+++ src/App.tsx
@@ -1,49 +1,25 @@
-import * as React from "react"
-import CenterFooter from "./components/globals/footer/CenterFooter"
-import {
-  ChakraProvider,
-  theme,
-} from "@chakra-ui/react"
-
-import Header from "./components/globals/header/Header"
-import { createBrowserRouter, RouterProvider } from "react-router-dom"
-
-import Home from "./pages/Home"
-import Contact from "./pages/Contact"
-import AboutMe from "./pages/AboutMe"
-import ProjectDetails from "./pages/ProjectDetails"
-import Projects from "./pages/Projects"
-
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Home />
-  },
-  {
-    path: "Contact",
-    element: <Contact />
-  },
-  {
-    path: "AboutMe",
-    element: <AboutMe />
-  },
-  {
-    path: "ProjectDetails/:id",
-    element: <ProjectDetails />
-  },
-  {
-    path: "Projects",
-    element: <Projects />
-  },
-]);
+import { ChakraProvider, theme } from '@chakra-ui/react';
+import { HashRouter, Route, Routes } from 'react-router-dom';
+import CenterFooter from './components/globals/footer/CenterFooter';
+import Header from './components/globals/header/Header';
+import AboutMe from './pages/AboutMe';
+import Contact from './pages/Contact';
+import Home from './pages/Home';
+import ProjectDetails from './pages/ProjectDetails';
+import Projects from './pages/Projects';
 
 export const App = () => (
   <ChakraProvider theme={theme}>
-    <Header></Header>
-    <RouterProvider router={router} />
-
-
-    <CenterFooter></CenterFooter>
+    <HashRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/Contact" element={<Contact />} />
+        <Route path="/AboutMe" element={<AboutMe />} />
+        <Route path="/ProjectDetails/:id" element={<ProjectDetails />} />
+        <Route path="/Projects" element={<Projects />} />
+      </Routes>
+      <CenterFooter />
+    </HashRouter>
   </ChakraProvider>
-)
+);
